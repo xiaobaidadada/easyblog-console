16398066b94051395c5074160199456115da8087
baoxiang <1643220580@qq.com>
2023-03-15 08:58:02 +0800

    iuou

--- src/components/header/HeaderXB.js
+++ src/components/header/HeaderXB.js
@@ -1,44 +1,42 @@
-
-
 import './header.css'
 
 /**
  * 登录状态组件
- * @param {} yy 
- * @returns 
+ * @param {} yy
+ * @returns
  */
 function State(yy) {
     return (
         <div>
-           <button>退出登录</button>
+            <button>退出登录</button>
         </div>)
 }
 
 /**
  * 编辑器修改状态
- * @param {显示保存js or css or 文章} yy 
- * @returns 
+ * @param {显示保存js or css or 文章} yy
+ * @returns
  */
 function Update(yy) {
     return (
         <div>
             <button>取消</button>
-           <button>确定修改{yy.name}</button>
+            <button>确定修改{yy.name}</button>
         </div>)
 }
 
 /**
  * 直接点击编辑
- * @param {}  
- * @returns 
+ * @param {}
+ * @returns
  */
 function Add() {
     return (
         <div>
             <button>取消</button>
-           <button>保存为css插件</button>
-           <button>保存为js插件</button>
-           <button>保存为文章</button>
+            <button>保存为css插件</button>
+            <button>保存为js插件</button>
+            <button>保存为文章</button>
         </div>)
 }
 
@@ -46,9 +44,14 @@ export default function HeaderXB(props) {
 
     return (
         <div id='header'>
-              
-                <State />
+
+            <div className="header_flag" >
+                EASYORM
+            </div>
+
+
+            {/*<State/>*/}
         </div>
-       
+
     );
 }
