07ab5f1b5fe06644abb042bdbf021f9e67c8399e
白保祥 <baoxiang1998@outlook.com>
2023-03-19 21:03:34 +0800

    部分更新

---
+++ src/components/config/requests.js
@@ -0,0 +1,27 @@
+import status from './config'
+
+function asy_get(path, query, handle) {
+
+    let result = {
+        code: "未知",
+        data: "无"
+    }
+
+    fetch(status.host + path + "?" + query, {
+        method: "GET",
+
+    }).then(response => response.json())
+        .then(json_data => {
+            //保证肯定是json数据
+            if (json_data['code'] == 1) {
+                result.data = json_data['value'];
+                result.code = "成功"
+
+            }
+            handle(result)
+
+        });
+
+}
+
+export default get;
